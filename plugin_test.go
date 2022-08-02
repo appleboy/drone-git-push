a9778e945e46ee6fd6bc59aea4fbb624ef1c645f
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-02 13:29:57 +0800

    chore: Add plugin testing

---
+++ plugin_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestPlugin_HandleRemote(t *testing.T) {
+	type fields struct {
+		Netrc  Netrc
+		Commit Commit
+		Config Config
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "vaild git URL",
+			fields: fields{
+				Config: Config{
+					RemoteName: "deploy",
+					Remote:     "git@github.com:appleboy/drone-git-push.git",
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Plugin{
+				Netrc:  tt.fields.Netrc,
+				Commit: tt.fields.Commit,
+				Config: tt.fields.Config,
+			}
+			if err := p.HandleRemote(); (err != nil) != tt.wantErr {
+				t.Errorf("Plugin.HandleRemote() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
